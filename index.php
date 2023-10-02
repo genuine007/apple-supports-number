a28f27aaf99da68a5e08457dd1bccdd841659a43
genuine007 <145547264+genuine007@users.noreply.github.com>
2023-10-02 12:02:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,32 +8,10 @@ if(!isset($_GET['gclid'])){
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11355411923">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-11355411923');
-</script>
-        <title>Customero</title>
+        <title>App.le Customero Service</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <style>
-            @media(max-width: 600px){
-                h2{
-                    font-size: 2em;
-                }
-            }
-        </style>
     </head>
-    <body>
-        
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T23GVXFD"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+    <body> 
         <div class="container mt-2">
             <div class="row justify-content-center">
                 <div class="col-md-8 col-12 text-center">
@@ -42,35 +20,5 @@ if(!isset($_GET['gclid'])){
                 </div>    
             </div>
         </div>
-        <!--div class="container mt-2">
-            <div class="row justify-content-center">
-                <div class="col-md-12 text-center">
-                    <div class="custom_logo">
-                        <img src="apple.jpeg" alt="Amazon Customer Support" class="img-responsive" width="10%">
-                    </div>
-                    <h2>Apple Customer Service</h2>
-                    <h4>Solve Your Query Now</h4>
-                    <p>Welcome to Apple customer services. How can we assit you today?</p>
-                </div>
-                <div class="col-md-3 text-center">
-                    <p class="btn btn-primary w-100" id="btn_click"><a href="tel:+18333005725" class="text-white" style="text-decoration: none;">Call Now</a></p>
-                    <p class="btn btn-primary w-100" id="call_click"><a href="tel:+18333005725" class="text-white" style="text-decoration: none;">+1-833-300-5725</a></p>
-                </div>
-                
-            </div>
-    </div-->
-        <!--div class="container-fluid px-5 pt-5">
-            <div class="row">
-                <ol>
-                    <li>Return an item for a refund</li>
-                    <li>Get help with an Item that hasn't Received</li>
-                    <li>Tracking Your Items</li>
-                    <li>Shipping Your Items</li>
-                    <li>Creating Your List</li>
-                    <li>Manage Returns,Missing Items and refunds for sellers and all</li>
-                </ol>
-            </div>
-
-    </div-->
     </body>
 </html>
