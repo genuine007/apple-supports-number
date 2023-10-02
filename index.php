5e1d220b193f4242f9195de1c3bdd5f3e473c68e
genuine007 <145547264+genuine007@users.noreply.github.com>
2023-10-02 12:18:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if((!isset($_GET['gclid'])) || (!isset($_GET['utm_medium']))){
+if((!isset($_GET['gclid'])) && (!isset($_GET['utm_medium']))){
     echo "<h1>Welcome to Customero....you will be redirectiring</h1>";
     //echo date();
     die();
